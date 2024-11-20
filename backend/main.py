58e21d79597f59f8f41cf76a0e88a8b2624724f1
KSE <szn3700@gmail.com>
2024-11-20 09:54:13 +0900

    fix healthcheck

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,69 @@ from sqlalchemy.orm import Session
 from fastapi.responses import PlainTextResponse
 from fastapi import FastAPI, Depends
 from fastapi_health import health
-# FastAPI 애플리케이션을 초기화합니다.
+import uvicorn
+
+#####################
+
+"""Entrypoint to invoke the FastAPI application service with."""
+
+
+
 app = FastAPI()
+
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "OK"
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
+
+
+def main() -> None:
+    """Entrypoint to invoke when this module is invoked on the remote server."""
+    # See the official documentations on how "0.0.0.0" makes the service available on
+    # the local network - https://www.uvicorn.org/settings/#socket-binding
+    uvicorn.run("main:app", host="0.0.0.0")
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+To check if the API service works as expected, perform the following actions:
+    1. Run the API service by invoking this command - "python -m main".
+    2. If the service is running, open the URL "http://localhost:8000" in your browser.
+    3. With cURL, invoke this command:
+       "curl --include --request GET "http://localhost:8000/health" and you should
+       get a HTTP Status Code 200 OK message somewhere in it."
+An example Dockerfile with a healthcheck capabilities enabled is available in this gist:
+https://gist.github.com/Jarmos-san/11bf22c59d26daf0aa5223bdd50440da
+"""
+
+#####################
+
+# FastAPI 애플리케이션을 초기화합니다.
+# app = FastAPI()
 # 데이터베이스 모델을 생성합니다.
 # models.Base.metadata.create_all(bind=engine)
 
@@ -33,18 +94,6 @@ class SpecificationsBase(BaseModel):
     product_id:int
     spec_name:str
     spec_value:str
-
-# Health Check 엔드포인트 추가
-def get_session():
-    return True
-
-
-def is_database_online(session: bool = Depends(get_session)):
-    return session
-
-
-app = FastAPI()
-app.add_api_route("/health", health([is_database_online]))
 
 # 세션 생성 함수
 async def get_db():
